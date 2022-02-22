9ad6c9eac6549ea3a1c3d9e17873732921c2960a
MichaelBash7 <bash-misha@mail.ru>
2022-02-22 18:16:01 +0300

    add SpringPotroshitelPractice

---
+++ spring-potroshitel/src/main/java/quoters/Main.java
@@ -0,0 +1,13 @@
+package quoters;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.print.attribute.standard.PrinterMoreInfo;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+        }
+    }
+
